24b9e883ecd45cceb57cdd36fe5a5e3ee22af000
newbe36524 <472158246@qq.com>
2019-09-22 17:37:50 +0800

    更新项目模板 1.14 -> 1.16

--- src/Newbe.Mahua.Template/Newbe.Mahua.Plugins.Template.MPQ/build.bat
+++ src/Newbe.Mahua.Template/Newbe.Mahua.Plugins.Template.MPQ/build.bat
@@ -1,4 +1,4 @@
+@echo off
 %~d0
 cd %~dp0
-buildTools\NuGet.exe install buildTools\packages.config -OutputDirectory packages -Verbosity normal
-powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {if(-not (Get-Module -ListAvailable  VSSetup)){Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser;Install-Module -Name VSSetup -Scope CurrentUser -Force;}Import-Module '.\packages\psake.*\tools\psake\psake.psd1';Import-Module '.\packages\Newbe.Build.Psake.*\tools\*.psm1'; invoke-psake .\build.ps1 %*; if ($LastExitCode -and $LastExitCode -ne 0) {write-host "ERROR CODE: $LastExitCode" -fore RED; exit $lastexitcode} }"
+powershell -NoProfile -NonInteractive -ExecutionPolicy unrestricted -Command "& {import-module .\buildScripts\psake\4.8.0\psake.psd1;import-module .\buildScripts\VSSetup\2.2.5\VSSetup.psd1;invoke-psake .\build.ps1 %*; if ($global:lastexitcode -and $global:lastexitcode -ne 0) {write-host "ERROR CODE: $global:lastexitcode" -fore RED; exit $global:lastexitcode} }"
